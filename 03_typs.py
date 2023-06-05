7100794761260f02d7a7cea9a7c082042babd21c
nposadaa <57275210+nposadaa@users.noreply.github.com>
2023-06-05 23:00:54 +0000

    Clase de types

--- 03_typs.py
+++ 03_typs.py
@@ -1,3 +1,22 @@
 # Strings: cadena de caracteres (texto)
-
 my_name = "Nicolás"
+my_name1 = "Lina"
+# texto entre comillas es para constantes en el valor impreso.
+intro = 'My name is ' + my_name + ', and my wife name is ' + my_name1
+print(intro)
+print(type(my_name))
+
+# integer, o números enteros.
+my_age = 36
+print(intro, 'and my age is', my_age)
+print(type(my_age))
+
+# booleanos, valos verdaderos o falsos.
+is_single = False
+print('Es soltero? ', is_single)
+print(type(is_single))
+
+# input da valorse a las variables
+my_age = input('Cuál es tu edad?')
+print('Mi edad es ', my_age)
+print(type(my_age))
