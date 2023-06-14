adea422290665e3db49774f1b303d0da85c37c2d
nposadaa <57275210+nposadaa@users.noreply.github.com>
2023-06-14 23:21:45 +0000

    Opeeradores de comparación

---
+++ 09_comparators.py
@@ -0,0 +1,29 @@
+# mayor qué '>'. Basicamente retorna un boleano con el resultado de la comparación.
+print('\n', 'Sección mayor qué')
+print('Resultado de 7 > 3:', 7 > 3)
+print('Resultado de 3 > 7:', 3 > 7)
+
+# menor qué '<'. Basicamente retorna un boleano con el resultado de la comparación.
+print('\n', 'sección menor qué')
+print('Resultado de 7 < 3:', 7 < 3)
+print('Resultado de 3 < 7:', 3 < 7)
+
+#mayor/menor igual qué '>=' agregamos el igual seguido al comparador de tamaño
+print('\n', 'sección mayor/igual qué')
+print('Resultado de 2 >= 2:', 2 >= 2)
+
+#operador de igualación para verifficar valores idénticos, ==.
+print('\n', 'sección igualdad')
+print('Resultado de 2 == 2:', 2 == 2)
+print('Resultado de 5 == 2:', 5 == 2)
+
+#operador de diferencia, !=.
+print('\n', 'sección diferencia')
+print('Resultado de 6 != 10:', 6 != 10)
+print('Resultado de 6 != 6:', 6 != 6)
+
+#Hay operadores que se pueden usar para strings también, tener presente que la comparación para que se objetiva debe hacerse entre tipos de datos iguales.
+
+print('\n', 'sección strings')
+print('Resultado de "Apple" == "Apple":', "Apple" == "Apple")
+print('Resultado de "Apple" == "apple":', "Apple" == "apple")
