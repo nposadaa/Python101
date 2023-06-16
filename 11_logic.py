308abf5eb4181a309f1fa76ffee1fb2edfa40221
nposadaa <57275210+nposadaa@users.noreply.github.com>
2023-06-16 00:37:43 +0000

    LOGIC for AND OR operators

---
+++ 11_logic.py
@@ -0,0 +1,25 @@
+# and: 
+print('''Example of how AND works by
+comparing to identical variables True:''','\n')
+print('True and True =>', True and True)
+print('True and False =>', True and False)
+
+print('\n','*' * 10,'\n')
+print('''Example of how AND works by
+comparing to identical INT variables:''','\n')
+print('10 > 5 and 5 < 10 =>', 10 > 5 and 5 < 10)
+print('10 < 5 and 5 > 10 =>', 10 < 5 and 5 > 10)
+print('10 < 5 and 5 < 10 =>', 10 < 5 and 5 < 10)
+
+print('\n','*' * 10,'\n')
+print('''Example of how OR works by
+comparing to booleans. OR will be always True as long something is true:''','\n')
+print('True or True =>', True or True)
+print('True or False =>', True or False)
+print('True or False =>', False or False)
+
+print('\n','*' * 10,'\n')
+print('''Example of how OR works by
+using an example of users giving their role:''','\n')
+role = input('Digita el rol => ')
+print(role == 'admin' or role == 'seller')
