53e6cdf1e9c7c3ca3b6f8e91ecae18b6ca4f03a8
nposadaa <57275210+nposadaa@users.noreply.github.com>
2023-07-05 23:37:18 +0000

    Modificación en clase 28 con tuplas

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-# Clase 20: proyecto piedra papel o tijera
+# Clase 20: proyecto piedra papel o tijera. Para reactivar esta opción, basta descomentar el bloque
+'''
+user_option = input('piedra, papel o tijera => ')
+user_option = user_option.lower()
+pc_option = 'piedra'
+
+if user_option == pc_option:
+  print('Empate!')
+elif user_option == 'piedra':
+  if pc_option == 'tijera':
+    print('piedra gana a Tijera!')
+    print('user ganó!')
+  else:
+    print('papel gana a piedra')
+    print('PC ganó!')
+elif user_option == 'papel':
+  if pc_option == 'piedra':
+    print('papel gana a piedra')
+    print('user ganó!')
+  else:
+    print('tijera gana a papel')
+    print('PC ganó!')
+elif user_option == 'tijera':
+  if pc_option == 'papel':
+    print('tijera gana a papel')
+    print('user ganó!')
+  else:
+    print('piedra gana a tijera')
+    print('PC ganó!')
+'''
+
+# clase 28: continuación del proyecto de piedra papel o tijera, incluyendo versión con tuplas
 
 user_option = input('piedra, papel o tijera => ')
 user_option = user_option.lower()
