4092c9728c72a99ea661f60a7acef13e18399fff
nposadaa <57275210+nposadaa@users.noreply.github.com>
2023-07-05 23:56:16 +0000

    Cambios con comentarios y validación del input del usuario

--- main.py
+++ main.py
@@ -30,11 +30,27 @@ elif user_option == 'tijera':
 '''
 
 # clase 28: continuación del proyecto de piedra papel o tijera, incluyendo versión con tuplas
+#Importamos la función random para usar en el programa
+import random
+
+#Esta es una tupla con las opcions para elegir
+options = ('piedra', 'papel', 'tijera')
 
 user_option = input('piedra, papel o tijera => ')
 user_option = user_option.lower()
-pc_option = 'piedra'
 
+#acá usamos la función random sacada de la tupla que declaramos anteriormente.
+pc_option = random.choice(options)
+
+#Mostramos las opciones elegidas, y procedemos a evaluar la lógica
+print('User option => ', user_option)
+print('PC option => ', pc_option)
+
+#Verificamos que la opción del usuario esté o no la opción digitada validando contra la tupla
+if not user_option in options:
+  print(f'El valor {user_option} no está en las opcions permitidas')
+
+#Decidimos el resultado: Empate, PC gana o user gana.
 if user_option == pc_option:
   print('Empate!')
 elif user_option == 'piedra':
